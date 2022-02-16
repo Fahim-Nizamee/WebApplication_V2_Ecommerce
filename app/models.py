ab15e1a0eaefe88ae710f7d53df2dbf878ea7573
Fahim-Nizamee <nizameef@gmail.com>
2022-02-16 11:21:55 +0600

    11:21 16/2/2022 First commit

---
+++ app/models.py
@@ -0,0 +1,62 @@
+from distutils.archive_util import make_zipfile
+from itertools import product
+from pyexpat import model
+from turtle import title
+from unicodedata import category, name
+from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django_countries.fields import CountryField
+
+
+
+class Customer(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    country = CountryField()
+    city = models.CharField(max_length=50)
+    zipcode = models.IntegerField()
+
+    def __str__(self):
+        return str(self.id)
+
+CATAGORY_CHOICES =(
+    ('M','Mobile'),
+    ('L','Laptop'),
+    ('TW','Top Wear'),
+    ('BW','Bottom Wear')
+)
+class Product(models.Model):
+    title=models.CharField(max_length=100)
+    selling_price=models.FloatField()
+    discounted_price=models.FloatField()
+    description = models.TextField()
+    brand=models.CharField(max_length=100)
+    category=models.CharField(choices=CATAGORY_CHOICES,max_length=2)
+    product_image=models.ImageField(upload_to='productimg')
+    def __str__(self):
+        return str(self.id)
+
+class Cart(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity=models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+
+STATUS_CHOICES =(
+    ('Accepted','Accepted'),
+    ('Packed','Packed'),
+    ('On The Way','On The Way'),
+    ('Delivered','Delivered'),
+    ('Cancel','Cancel')
+)
+class OrderPlaced(models.Model):
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
+    customer=models.ForeignKey(Customer,on_delete=models.CASCADE)
+    product=models.ForeignKey(Product,on_delete=models.CASCADE)
+    quantity=models.PositiveIntegerField(default=1)
+    ordered_date=models.DateTimeField(auto_now_add=True)
+    status=models.CharField(max_length=50,choices=STATUS_CHOICES,default='Pending')
+
