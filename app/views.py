08fff9543a5549394dab8ffe5ed38528cf4851ad
Fahim-Nizamee <nizameef@gmail.com>
2022-02-16 12:19:13 +0600

    12:18 16/2/2022 Second commit

--- app/views.py
+++ app/views.py
@@ -1,7 +1,19 @@
+from unicodedata import category
 from django.shortcuts import render
+from django.views import View
+from .models import Customer,Product,Cart,OrderPlaced
 
-def home(request):
- return render(request, 'app/home.html')
+
+# def home(request):
+#  return render(request, 'app/home.html')
+
+class ProductView(View):
+    def get(self,request):
+        topwears = Product.objects.filter(category='TW')
+        bottomwears = Product.objects.filter(category='BW')
+        mobiles = Product.objects.filter(category='M')
+        laptops = Product.objects.filter(category='L')
+        return render(request,'app/home.html',{'topwears':topwears,'bottomwears':bottomwears,'mobiles':mobiles,'laptops':laptops})
 
 def product_detail(request):
  return render(request, 'app/productdetail.html')
