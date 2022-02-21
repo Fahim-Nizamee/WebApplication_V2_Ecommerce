1863f339a12cdc6aff037edb230cffc3534c556a
Fahim-Nizamee <nizameef@gmail.com>
2022-02-21 22:09:40 +0600

    10:09 21/2/2022

--- Store/settings.py
+++ Store/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL= '/media/'
 MEDIA_ROOT= BASE_DIR/'media'
-LOGIN_REDIRECT_URL='/profile/'
+LOGIN_REDIRECT_URL='/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
